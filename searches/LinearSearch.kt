7c0235d559801b0c2bf051bec3c1dd1bb18863cc
Roman Korostenskyi <romankorostensky@gmail.com>
2019-10-04 17:11:57 +0300

    refactor: improve linear search algorithm implementation

--- searches/LinearSearch.kt
+++ searches/LinearSearch.kt
@@ -1,9 +1,20 @@
-fun <T> List<T>.searchLinear(value: T): Boolean {
-    this.forEach { if (it == value) return true }
-    return false
+/**
+ * @return index of element. -1 if not found or list is empty
+ */
+fun <T> List<T>.searchLinear(
+    value: T,
+    fromIndex: Int = 0,
+    toIndex: Int = size
+): Int {
+    takeIf { it.isNotEmpty() }
+    for (i: Int in fromIndex until toIndex) if (this[i] == value) return i
+    return -1
 }
 
 fun main() {
-    println(listOf(1, 2, 3, 4, 5).searchLinear(3)) // -> false
-    println(listOf(1, 2, 3, 4, 5).searchLinear(6)) // -> false
+    val list = listOf(1, 2, 3, 4, 5)
+    println(list.searchLinear(3)) // -> 2
+    println(list.searchLinear(6)) // -> -1
+    println(list.searchLinear(4, 3, 9)) // -> 3
+    println(listOf<Int>().searchLinear(5)) // -> -1
 }
