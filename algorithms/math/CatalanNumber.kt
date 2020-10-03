bb4260f71c3c619bbd9363e803c8230765505c04
Mantas <mantas84@gmail.com>
2020-10-03 12:32:05 +0300

    Catalan number

---
+++ algorithms/math/CatalanNumber.kt
@@ -0,0 +1,24 @@
+import java.math.BigInteger
+
+/*
+https://en.wikipedia.org/wiki/Catalan_number
+ */
+
+fun findFirst16(){
+    for (i in 0..15){
+        val number = catalanNumber(i)
+        println("$number")
+    }
+}
+
+fun catalanNumber(nThNumber: Int): BigInteger {
+    val numerator = factorial(2 * nThNumber)
+    val denominator = factorial(nThNumber + 1).multiply(factorial(nThNumber))
+    return numerator.divide(denominator)
+}
+
+fun factorial(n: Int): BigInteger {
+    var result = BigInteger.ONE
+    for (i in 2..n) result = result.multiply(BigInteger.valueOf(i.toLong()))
+    return result
+}
